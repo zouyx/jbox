181598933946077ee98f65551302c7797292a303
Joe Zou <yixian.zou@gmail.com>
2018-10-31 19:38:03 +0800

    增加接口返回结构

---
+++ src/main/java/com/tevid/jbox/controller/response/RestReturn.java
@@ -0,0 +1,33 @@
+package com.tevid.jbox.controller.response;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.tevid.jbox.contant.Const;
+
+@JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
+public class RestReturn<T> extends Return {
+    public RestReturn() {
+    }
+
+    private T data;
+
+    public RestReturn(T data) {
+        this(Const.RETURN_CODE.SUCCESS.value,data);
+    }
+
+    public RestReturn(int code, T data) {
+        super(code);
+        this.data=data;
+    }
+    public RestReturn(int code, String msg, T data){
+        super(code,msg);
+        this.data=data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
